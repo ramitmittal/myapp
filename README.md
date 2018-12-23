3455bac961216e1b059b43cb57a2a12185c73680
Rahul Soni <rahul@attosol.com>
2018-12-23 09:57:22 +0530

    Remove date-fns and add Moment library Modified greeting functionality

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # myapp
 Simple web server written in Node for Up and running with Git article series. Find the accompanying article [here](https://www.attosol.com/getting-started-with-git-part-1/).
+
+### Modified the greeting
+Now we greet on all the days!
--- handler.js
+++ handler.js
@@ -1,24 +1,25 @@
-const dfns = require('date-fns');
 const url = require('url');
+const moment = require('moment');
 
 let handler = (req, res) => {
-    let queryData = url.parse(req.url, true).query;
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    
-    let greetString = "";
-    if (queryData.name) {
-	greetString = "Hello user!\n";
-    }
-    else {
-	greetString = "Hello World!\n";
-    }
-    let friday = dfns.isFriday(new Date());
-    if (friday) {
-	greetString += "It's Friday!"
-    }
-    res.end(greetString);
+  let queryData = url.parse(req.url, true).query;
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
 
-}
+  let greetString = "";
+  if (queryData.name) {
+    greetString = "Hello user!\n";
+  } else {
+    greetString = "Hello World!\n";
+  }
+
+  let day = moment().format('dddd');
+  if (day === "Friday") {
+    greetString += "Thank God! It is FRIDAY!"
+  } else {
+    greetString += ("It's " + day);
+  }
+  res.end(greetString);
+};
 
 module.exports = handler;
